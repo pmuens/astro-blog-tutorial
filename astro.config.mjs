8412092e01ba68c922f77701578fdb05ce64972e
Philipp Muens <philipp@muens.io>
2024-03-07 14:26:58 +0100

    Add Link component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+});
