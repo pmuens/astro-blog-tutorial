2f856b7c6cdf9f031cb2570b08ea22871b5b1961
Philipp Muens <philipp@muens.io>
2024-03-08 14:18:00 +0100

    Add Tailwind CSS example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import icon from "astro-icon";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-cip.netlify.app",
-  integrations: [icon(), sitemap(), react()],
+  integrations: [icon(), sitemap(), react(), tailwind()],
 });
